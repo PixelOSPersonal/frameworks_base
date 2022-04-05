13f1e71bdff10ea7472763f1742157de6d94ece7
jhonboy121 <alfredmathew05@gmail.com>
2022-04-05 12:14:45 +0530

    base: allow hiding apps from launcher [1/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Iefafa7cb148700d1d7667e07b857217d286004bd
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11612,6 +11612,12 @@ public final class Settings {
          */
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
+	/**
+         * List of ; separated apps to be hidden from launcher.
+         * @hide
+         */
+        public static final String LAUNCHER_HIDDEN_APPS = "launcher_hidden_apps";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
