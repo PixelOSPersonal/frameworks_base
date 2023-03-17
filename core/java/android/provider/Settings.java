c13eaf17b2e9b94ad27de0f0d37d652818a2b155
Bruno Martins <bgcngm@gmail.com>
2023-03-17 09:35:44 +0000

    SystemUI: Bring back good ol' circle battery style
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   2019-11-30 13:41:05 +0100
        SystemUI: Bring back good ol' circle battery style
    
        * This code is based on the old Java implementation and converted to Kotlin
    
        * Instead of creating different style within the same drawable
          create one on its own, this makes the code more readable and future proof
    
        Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2021-04-08 21:41:24 +0100
        SystemUI: Fix status bar battery icon title
    
        The wrong string was being picked up which is not capitalized
        and not even localized. Use the already localized string
        from SystemUI package.
    
        Change-Id: I4be427c95a0fb98c77d450e0791bb8e3442ed3a1
    
    Author: althafvly <althafvly@gmail.com>
    Date:   Mon Jul 11 15:13:40 2022 +0530
        SystemUI: Redo circle battery style
    
        - Now shows up on QuickSettings panel.
        - Fixes issues with battery circle style on theme and
          ui mode change and statusbar icon toggle.
        - Fixes wrong tint color with circle battery icon.
        - Re-evaluating system theme to avoid delay in circle
          battery tint color change.
    
        Change-Id: Id90f85e47ce4b9811e9bb67b91169997020300d8
    
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5826,6 +5826,26 @@ public final class Settings {
         public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
+                "status_bar_show_battery_percent";
+
+        /**
          * Whether to enable smart 5G mode
          * @hide
          */
@@ -5954,6 +5974,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(STATUS_BAR_AM_PM);
+            PRIVATE_SETTINGS.add(STATUS_BAR_BATTERY_STYLE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_SHOW_BATTERY_PERCENT);
         }
 
         /**
--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -76,6 +76,7 @@ import com.android.systemui.statusbar.policy.ConfigurationController.Configurati
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.util.settings.SecureSettings;
+import com.android.systemui.util.settings.SystemSettings;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -115,6 +116,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private final BroadcastDispatcher mBroadcastDispatcher;
     private final Executor mBgExecutor;
     private final SecureSettings mSecureSettings;
+    private final SystemSettings mSystemSettings;
     private final Executor mMainExecutor;
     private final Handler mBgHandler;
     private final boolean mIsMonochromaticEnabled;
@@ -385,7 +387,8 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             FeatureFlags featureFlags,
             @Main Resources resources,
             WakefulnessLifecycle wakefulnessLifecycle,
-            ConfigurationController configurationController) {
+            ConfigurationController configurationController,
+            SystemSettings systemSettings) {
         mContext = context;
         mIsMonochromaticEnabled = featureFlags.isEnabled(Flags.MONOCHROMATIC_THEME);
         mIsMonetEnabled = featureFlags.isEnabled(Flags.MONET);
@@ -398,6 +401,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mBgHandler = bgHandler;
         mThemeManager = themeOverlayApplier;
         mSecureSettings = secureSettings;
+        mSystemSettings = systemSettings;
         mWallpaperManager = wallpaperManager;
         mUserTracker = userTracker;
         mResources = resources;
@@ -435,6 +439,32 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                             return;
                         }
                         reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
+        mSystemSettings.registerContentObserverForUser(
+                Settings.System.getUriFor(Settings.System.STATUS_BAR_BATTERY_STYLE),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        if (DEBUG) Log.d(TAG, "Overlay changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        boolean isCircleBattery = Settings.System.getIntForUser(
+                                mContext.getContentResolver(),
+                                Settings.System.STATUS_BAR_BATTERY_STYLE,
+                                0, UserHandle.USER_CURRENT) == 1;
+                        if (isCircleBattery) {
+                            reevaluateSystemTheme(true /* forceReload */);
+                        }
                     }
                 },
                 UserHandle.USER_ALL);
